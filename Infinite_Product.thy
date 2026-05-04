ef458871e501fc63f7be77cdaf8501b7cfd79eb5
Lawrence Paulson <lawrpau@amazon.com>
2026-05-04 22:32:56 +0100

    progress

--- Infinite_Product.thy
+++ Infinite_Product.thy
@@ -1073,20 +1073,65 @@ proof -
 
   \<comment> \<open>Step 3: The map G \<mapsto> G \<union> FA embeds finite_subsets_at_top B into finite_subsets_at_top A (eventually).\<close>
   have embed: \<open>\<forall>\<^sub>F G in finite_subsets_at_top B. finite (G \<union> FA) \<and> (G \<union> FA) \<subseteq> A \<and> F0 \<subseteq> (G \<union> FA)\<close>
-    sorry
+  proof (subst eventually_finite_subsets_at_top, rule exI[of _ \<open>F0 \<inter> B\<close>], intro conjI)
+    show \<open>finite (F0 \<inter> B)\<close> using F0_fin by auto
+    show \<open>F0 \<inter> B \<subseteq> B\<close> by auto
+    show \<open>\<forall>G. finite G \<and> F0 \<inter> B \<subseteq> G \<and> G \<subseteq> B \<longrightarrow> finite (G \<union> FA) \<and> (G \<union> FA) \<subseteq> A \<and> F0 \<subseteq> (G \<union> FA)\<close>
+    proof (intro allI impI, elim conjE)
+      fix G assume \<open>finite G\<close> \<open>F0 \<inter> B \<subseteq> G\<close> \<open>G \<subseteq> B\<close>
+      show \<open>finite (G \<union> FA) \<and> (G \<union> FA) \<subseteq> A \<and> F0 \<subseteq> (G \<union> FA)\<close>
+        using \<open>finite G\<close> \<open>G \<subseteq> B\<close> \<open>B \<subseteq> A\<close> \<open>F0 \<inter> B \<subseteq> G\<close> FA_sub FA_fin
+        by (auto simp: FA_def)
+    qed
+  qed
 
   \<comment> \<open>Step 4: So G \<mapsto> prod f (G \<union> FA) converges along finite_subsets_at_top B.\<close>
   have conv_union: \<open>\<exists>T. ((\<lambda>G. prod f (G \<union> FA)) \<longlongrightarrow> T) (finite_subsets_at_top B)\<close>
-    sorry
+  proof -
+    have BFA_sub: \<open>B \<union> FA \<subseteq> A\<close> using \<open>B \<subseteq> A\<close> FA_sub by auto
+    have mono: \<open>finite_subsets_at_top (B \<union> FA) \<le> finite_subsets_at_top A\<close>
+      unfolding le_filter_def
+    proof (intro allI impI)
+      fix P assume \<open>eventually P (finite_subsets_at_top A)\<close>
+      then obtain X where \<open>finite X\<close> \<open>X \<subseteq> A\<close> 
+        and XP: \<open>\<forall>Y. finite Y \<and> X \<subseteq> Y \<and> Y \<subseteq> A \<longrightarrow> P Y\<close>
+        unfolding eventually_finite_subsets_at_top by auto
+      show \<open>eventually P (finite_subsets_at_top (B \<union> FA))\<close>
+        unfolding eventually_finite_subsets_at_top
+        apply (rule exI[of _ X])
+        using \<open>finite X\<close> \<open>X \<subseteq> A\<close> BFA_sub XP by auto
+    qed
+    have lim_BFA: \<open>(prod f \<longlongrightarrow> S) (finite_subsets_at_top (B \<union> FA))\<close>
+      using limS mono tendsto_mono by blast
+    have filt: \<open>filterlim (\<lambda>G. G \<union> FA) (finite_subsets_at_top B) (finite_subsets_at_top (B \<union> FA))\<close>
+      using filterlim_Un_finite_subsets_at_top[OF FA_fin] by simp
+    have \<open>((\<lambda>G. prod f (G \<union> FA)) \<longlongrightarrow> S) (finite_subsets_at_top B)\<close>
+      using filterlim_compose[OF lim_BFA filt] by (simp add: comp_def)
+    then show ?thesis by auto
+  qed
 
   \<comment> \<open>Step 5: Since prod f (G \<union> FA) = prod f G * prod f FA and prod f FA \<noteq> 0,
      we get prod f G = prod f (G \<union> FA) / prod f FA.\<close>
   have eq: \<open>\<forall>\<^sub>F G in finite_subsets_at_top B. prod f G = prod f (G \<union> FA) / prod f FA\<close>
-    sorry
+  proof (rule eventually_finite_subsets_at_top_weakI)
+    fix G assume \<open>finite G\<close> \<open>G \<subseteq> B\<close>
+    from split[OF this] FA_nz show \<open>prod f G = prod f (G \<union> FA) / prod f FA\<close>
+      by (simp add: field_simps)
+  qed
 
   \<comment> \<open>Step 6: Dividing a convergent net by a nonzero constant gives a convergent net.\<close>
   have conv_B: \<open>\<exists>T. (prod f \<longlongrightarrow> T) (finite_subsets_at_top B)\<close>
-    sorry
+  proof -
+    from conv_union obtain T where convT: \<open>((\<lambda>G. prod f (G \<union> FA)) \<longlongrightarrow> T) (finite_subsets_at_top B)\<close>
+      by auto
+    have \<open>((\<lambda>G. prod f (G \<union> FA) / prod f FA) \<longlongrightarrow> T / prod f FA) (finite_subsets_at_top B)\<close>
+      using tendsto_divide[OF convT tendsto_const FA_nz] by simp
+    moreover have \<open>\<forall>\<^sub>F G in finite_subsets_at_top B. prod f (G \<union> FA) / prod f FA = prod f G\<close>
+      using eq by (rule eventually_mono) auto
+    ultimately have \<open>(prod f \<longlongrightarrow> T / prod f FA) (finite_subsets_at_top B)\<close>
+      by (rule Lim_transform_eventually)
+    then show ?thesis by auto
+  qed
 
   \<comment> \<open>Step 7: Hence f multipliable_on B.\<close>
   then show \<open>f multipliable_on B\<close>
